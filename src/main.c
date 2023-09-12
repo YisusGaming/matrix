bce7b8afcc88f2ea33abbbda8e8b4604367f9f05
Yisus <jesusgameroom@gmail.com>
2023-09-11 21:59:49 -0400

    printing with colors

--- src/main.c
+++ src/main.c
@@ -6,10 +6,18 @@
 
 int main(int argc, char const *argv[])
 {
-    while(1 == 1) {
+    char *colors[] = {
+        /* reset */ "\033[1;0m", 
+        /* green */ "\033[1;32m", 
+        /* magenta */ "\033[1;35m",
+        /* cyan */ "\033[1;36m",
+        /* yellow */ "\033[1;33m"
+    };
 
+    while(1 == 1) {
         char* row = generate_random_row();
-        printf("%s", row);
+        int i_color = rand() % 5;
+        printf("%s%s%s", colors[i_color], row, colors[0]);
 
         free(row); // if this thing has memory leaks I'm gonna cry
     }
